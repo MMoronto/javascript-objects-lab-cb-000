8251f6c9130b6f75ebfb93a18529f50b85b27190
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-27 01:46:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, {[key]: value})
   
-  delete object.key
+  delete object[key]
   return object
 }
 
 function deleteFromObjectByKey(object, key) {
   const newObj = Object.assign({}, object)
   
-  delete newObj.key
+  delete newObj[key]
   return newObj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
