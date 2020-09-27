af185fc298a67ae20c788a15887617d3dc75bf5d
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-27 01:42:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function deleteFromObjectByKey(object, key) {
   return object
 }
 
-function deleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key
   return object
 }
